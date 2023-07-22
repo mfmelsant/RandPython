8fb0d3a4b7fe24cf1f2986729602d3ef31fec529
Fernanda <mfmelchiorsantarosa@gmail.com>
2023-07-21 21:34:08 -0300

    ex 1 and 2 -R

---
+++ desafio1.R
@@ -0,0 +1,13 @@
+#do it yourself:
+#1- Create two variables Ana and Paulo and assign to them values of their ages, 
+#being respectively 8 and 12 years. The variables must be of integer type. Print on the console
+#'The girl is older' or 'The boy is older' as a result of a logical test.
+
+Ana=8
+Paulo=12
+x=ifelse(Paulo>Ana, "The boy is older", "The girl is older")
+x 
+
+#2- BOD is an existing object in R. Check what class this object is.
+#3-Create a vector that stores numbers from 1 to 10 and later print the even values
+#4-Women is an existing dataframe in R. Print the last 10 lines
---
+++ readme.md
@@ -0,0 +1,3 @@
+This repository is used to store my study codes about R language and Python for data science.
+My name is Maria Fernanda and I am a data science student at UNINTER - Brazil
+Feel free to follow my progress with the codes stored here, named as challenges, each .py or .r file has the order from 1 to (?) with a code file for each challenge.
