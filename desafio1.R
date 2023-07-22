907836616adc5670beb25475f60899111bf3a992
Fernanda <mfmelchiorsantarosa@gmail.com>
2023-07-21 21:35:01 -0300

    R

--- desafio1.R
+++ desafio1.R
@@ -7,7 +7,15 @@ Ana=8
 Paulo=12
 x=ifelse(Paulo>Ana, "The boy is older", "The girl is older")
 x 
+#The code doesn´t work in VSCode, but work in RStudio
 
 #2- BOD is an existing object in R. Check what class this object is.
+
+class(BOD)
+
 #3-Create a vector that stores numbers from 1 to 10 and later print the even values
+
+x = c(1,2,3,4,5,6,7,8,9,10)
+if {}
+
 #4-Women is an existing dataframe in R. Print the last 10 lines
